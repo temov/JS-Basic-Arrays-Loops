71f1b25866fd88ffc20ea2909c1d67576a356f1f
Tode Temov <75773817+temov@users.noreply.github.com>
2022-12-25 01:42:25 +0100

    Add files via upload

--- Homework_4/script.js
+++ Homework_4/script.js
@@ -1,13 +1,11 @@
 
 function list(){
   
-   for(let i =1; i<=20; i++){
-      if (i % 2 === 0){
-         console.log(i +' '+ '<br/>');
-      }else{
-         console.log(i +' '+ '&nbsp');
-      }
-   
+   for(let i =0; i< 20; i++){
+      
+         i++;
+     
+      console.log( i+' '+ (i+1) +'\n');
    }
    
    
