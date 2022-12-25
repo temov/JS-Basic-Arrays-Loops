2be6aeb02f45668b4f237d18443a293409ac6efd
Tode Temov <75773817+temov@users.noreply.github.com>
2022-12-25 01:51:17 +0100

    Add files via upload

--- Homework_6/script.js
+++ Homework_6/script.js
@@ -3,16 +3,11 @@
 
 
 function firstLastNames(array1,array2){
-   let newArray1 = [];
-   let newArray2 = [];
    let fullName = [];
    let counter=1;
    for(let i=0; i < array1.length; i++){
         
-        newArray1.push(`${counter}.${array1[i]}`);
-        newArray2.push(`${array2[i]}`);
-        
-        fullName.push(`${newArray1[i]} ${newArray2[i]}`);
+        fullName.push(`${counter}.${array1[i]} ${array2[i]}`);
         counter ++;
    }
    console.log(fullName);
